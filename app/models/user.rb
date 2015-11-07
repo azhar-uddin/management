5aa9680b22584beb8eba612efa9b05b92d065904
Azhar <mohammad.azhar2000@gmail.com>
2015-11-08 04:32:57 +0530

    some bugs fix1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_many :results
   accepts_nested_attributes_for :results
   validates_inclusion_of :gender, :in => %w( m f M F)
+  validates :rollnumber, uniqueness: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
